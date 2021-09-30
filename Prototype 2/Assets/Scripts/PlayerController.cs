2022f648d557403e18503cb725ddd4f6471cf33d
Paras-Dubey <87394952+Paras-Dubey@users.noreply.github.com>
2021-10-01 00:36:53 +0530

    Update PlayerController.cs
    Add boundary limit on the player's horizontal movement.

--- Prototype 2/Assets/Scripts/PlayerController.cs
+++ Prototype 2/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 
     public float horizontalInput;
     public float speed = 10.0f;
+    public float range = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -17,6 +18,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Limit player movements to the boundries
+        if(transform.position.x < -range)
+        {
+            transform.position = new Vector3(-range, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x > range)
+        {
+            transform.position = new Vector3(range, transform.position.y, transform.position.z);
+        }
+
         //Get horizontal input
         horizontalInput = Input.GetAxis("Horizontal");
 
